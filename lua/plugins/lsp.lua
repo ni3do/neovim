6b677ed9a54f1dfb549ba59af368c2368efb3a8a
niedo <57731234+ni3do@users.noreply.github.com>
2025-05-07 16:56:31 +0200

    auto-commit

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -238,7 +238,8 @@ return {
 		-- for you, so that they are available from within Neovim.
 		local ensure_installed = vim.tbl_keys(servers or {})
 		vim.list_extend(ensure_installed, {
-			"stylua", -- Used to format Lua code
+			"stylua", -- lua coder formatter
+			"nixfmt", -- nix coder formatter
 		})
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
